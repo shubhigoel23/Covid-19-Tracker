1d7e3e9bfd6c12951dbf3584bf8b791554cc740b
Shubhi Goel <iamshubhigoel@gmail.com>
2021-11-08 12:03:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,73 +10,94 @@ var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/dark-v10'
 ,zoom: 1.5 ,
-center:[0,20],
-//  fill-antialias:true,
-//  fill-color: white
+center:[0,20]
 });
 // for dark mode of the map: change "streets-v11"  to "dark-v10"
 
-fetch("https://covid-193.p.rapidapi.com/statistics", {
+const getColorFromCount = (country_population,confirmed)=>{
+const fiftyPercent=(0.5*country_population)
+  if((0.7*country_population)<confirmed)
+  return "red";
+  else if (fiftyPercent<=confirmed)
+  return "blue";
+  else
+  return "grey";
+}
+
+fetch("https://coronavirus-tracker-api.herokuapp.com/v2/locations", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "covid-193.p.rapidapi.com",
-		"x-rapidapi-key": "40f3d2b5fdmsh525a683ae9d53ebp182319jsn48aaa8d52a9d"
+    'Content-Type':'application/json'
 	}
 })
 .then(response => response.json())
-.then(data=>{
-  const response=data.response;
+.then(data =>{
+  const latest=data.latest;
+  console.log(data);
+  console.log(latest);
+  //for accesing latest total number of cases worldwide
 
-    // console.log(response);
-    // for extracting data from wihin response array
+  const locations=data.locations;
+  console.log(locations);
+  // for accessing the loactions arrray
 
+  // locations
+  // .filter(locations=>{
+  //   const country=locations.country;
+  //   return country=='Afghanistan';
+  // })
+  // .forEach(location => {
+  //   console.log(location); 
+  // });
+  // //for filtering out data of afghanistan
 
-    // console.log(data.response[4]);
-    // for extracting data from wihin response array element 4
+  locations
+  .forEach(location => {
+    const coordinates=location.coordinates;
+    const{latitude,longitude}=coordinates;
+    const latest=location.latest;
+    const {confirmed,deaths}=latest;
+    const {country_population}=location;
+   new mapboxgl.Marker({
+     color:getColorFromCount(country_population,confirmed)
+   })
+      .setLngLat([longitude,latitude])
+    
+      .addTo(map)
+/* 
+Add an event listener that runs
+  when a user clicks on the map element.
+*/
+.addTo(map);
+map.on('click', (event) => {
+  // If the user clicked on one of your markers, get its information.
+  const features = map.queryRenderedFeatures(event.point, {
+    layers: ['mapbox://styles/mapbox/dark-v10'] // replace with your layer name
+  });
+  if (!features.length) {
+    return;
+  }
+  const feature = features[0];
 
+  // Code from the next step will go here.
+  /* 
+    Create a popup, specify its options 
+    and properties, and add it to the map.
+  */
+    const popup = new mapboxgl.Popup({ offset: [0, -15] })
+    .setLngLat([longitude,latitude])
+    .setHTML(
+      `<h4>Cases</h4>
+      <h3>
+      <ul>
+      <li>Confirmed:${confirmed}</li>
+      <li>Deaths:${deaths}</li>
+      </ul></h3>`
+    )
 
-    // response.forEach(response => {
-    //   console.log(response);
-    // });
-    // for traversing the array and logging it into the console
-
-    response
-    .filter(response=>{
-      const continent=response.continent;
-      return continent=="Asia";
-    })
-    .forEach(response => {
-      console.log(response);
-    });
-    // filters out all the countries with continent=Asia
-
+});      
+  });
 } )
 .catch(err => {
 	console.error(err);
 });
-
-
-
-
-
-
-
-// 2. more latency and less popularity (pink logo)
-// fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
-// 		"x-rapidapi-key": "40f3d2b5fdmsh525a683ae9d53ebp182319jsn48aaa8d52a9d"
-// 	}
-// })
-// .then(response => response.json())
-// .then(data=>{
-//     console.log(data);
-// } )
-// .catch(err => {
-// 	console.error(err);
-// });
-
-
-
-
