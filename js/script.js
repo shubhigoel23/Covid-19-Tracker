70a21e3e393305ae351b56a3ba9f9ea47b67b45d
Shubhi Goel <iamshubhigoel@gmail.com>
2021-11-19 11:43:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ function myFunction(x) {
 }
 
 const mapBox_token="pk.eyJ1Ijoic2h1YmhpZ29lbDIwIiwiYSI6ImNrdmNybHg4cjZyd20zMHM3bzhiZTlneXoifQ.vkGRaV8Vb94Z_KcffajC0Q";
-
 mapboxgl.accessToken = mapBox_token;
 var map = new mapboxgl.Map({
 container: 'map',
@@ -17,13 +16,14 @@ center:[0,20]
 const getColorFromCount = (country_population,confirmed)=>{
 const fiftyPercent=(0.5*country_population)
   if((0.7*country_population)<confirmed)
-  return "red";
+  return "#f44336";
   else if (fiftyPercent<=confirmed)
-  return "blue";
+  return "#009688";
   else
   return "grey";
 }
 
+//Fetching data 
 fetch("https://coronavirus-tracker-api.herokuapp.com/v2/locations", {
 	"method": "GET",
 	"headers": {
@@ -39,63 +39,45 @@ fetch("https://coronavirus-tracker-api.herokuapp.com/v2/locations", {
 
   const locations=data.locations;
   console.log(locations);
-  // for accessing the loactions arrray
-
-  // locations
-  // .filter(locations=>{
-  //   const country=locations.country;
-  //   return country=='Afghanistan';
-  // })
-  // .forEach(location => {
-  //   console.log(location); 
-  // });
-  // //for filtering out data of afghanistan
 
   locations
   .forEach(location => {
     const coordinates=location.coordinates;
     const{latitude,longitude}=coordinates;
     const latest=location.latest;
-    const {confirmed,deaths}=latest;
-    const {country_population}=location;
+    const {confirmed,deaths,recovered}=latest;
+    const {country_population,country,province}=location;
+
+//Creating a pop-up
+    const Popup = new mapboxgl.Popup({ offset: [0, -15]})
+    .setHTML(
+      `
+      <h3 style="text-align:center; text-decoration: underline; font-size:18px">${country}</h3>
+    <h3 style="color:#582C4D;">Province: ${province}</h3>
+      <h4>
+      <ul style=" list-style-type: none">
+      <li style="color:#aa4465 ">Confirmed: ${confirmed}</li>
+      <li style="color:#0B3132 ">Recovered: ${recovered}</li>
+      <li style="color:#d00000 ">Deaths: ${deaths}</li>
+      </ul></h4>`
+    // `<div style="background-color:#efd3d7 "><h3 style="text-align:center; text-decoration: underline; font-size:18px">${country}</h3>
+    // <h3 style="color:#582C4D">Province: ${province}</h3>
+    // <h4>
+    // <ul>
+    // <li style="color:#a53860 ">Confirmed: ${confirmed}</li>
+    // <li style="color:#0B3132 ">Recovered: ${recovered}</li>
+    // <li style="color:#d00000 ">Deaths: ${deaths}</li>
+    // </ul></h4></div>`
+    );
+
+// Creating a marker and attaching pop-up
    new mapboxgl.Marker({
      color:getColorFromCount(country_population,confirmed)
    })
       .setLngLat([longitude,latitude])
-    
+      .setPopup(Popup)
       .addTo(map)
-/* 
-Add an event listener that runs
-  when a user clicks on the map element.
-*/
-.addTo(map);
-map.on('click', (event) => {
-  // If the user clicked on one of your markers, get its information.
-  const features = map.queryRenderedFeatures(event.point, {
-    layers: ['mapbox://styles/mapbox/dark-v10'] // replace with your layer name
-  });
-  if (!features.length) {
-    return;
-  }
-  const feature = features[0];
 
-  // Code from the next step will go here.
-  /* 
-    Create a popup, specify its options 
-    and properties, and add it to the map.
-  */
-    const popup = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat([longitude,latitude])
-    .setHTML(
-      `<h4>Cases</h4>
-      <h3>
-      <ul>
-      <li>Confirmed:${confirmed}</li>
-      <li>Deaths:${deaths}</li>
-      </ul></h3>`
-    )
-
-});      
   });
 } )
 .catch(err => {
